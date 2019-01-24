08943dd8d1a723796d19d5ff637aac97acffacec
Gabriel FM <gabrielfm5566@gmail.com>
2019-01-24 18:48:33 +0000

    Add folder gets Folder directory

--- Discord Data Package Parser/MainWindow.xaml.cs
+++ Discord Data Package Parser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Discord_Data_Package_Parser
 {
@@ -20,9 +22,51 @@ namespace Discord_Data_Package_Parser
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+
         public MainWindow()
         {
             InitializeComponent();           
+        }
+
+        private void MenuOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                EnsurePathExists = true,
+                EnsureFileExists = false,
+                AllowNonFileSystemItems = false,
+                DefaultFileName = "Select the Package Folder",
+                Title = "Select The Folder To Parse"
+            };
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+            
+            }
+
+        }
+
+        private void MenuOpenZIP_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuTheme_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Will be added soon :)", "Not available", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Clear()
+        {
+            lstDM.Items.Clear();
+            lstServers.Items.Clear();
         }
     }
 }
---
+++ Discord Data Package Parser/functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord_Data_Package_Parser;
+
+namespace Discord_Data_Package_Parser
+{
+    class functions
+    {
+        private void ClearTabs()
+        {
+           
+        }
+    }
+}
